c58f55d2dd25c22d03e7db7a0e1018cf775fe335
Giuseppe <giuseppe.quartarone@digital.hmrc.gov.uk>
2017-06-05 00:19:11 +0100

    Add subnets specs

---
+++ spec/destroy/subnets_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'yaml'
+require 'json'
+include Awspec::Helper::Finder
+
+json = JSON.parse(File.read("/tmp/vars.json"))
+
+subnets = json['subnets']
+
+describe "Subnets" do
+  unless subnets.nil? || subnets == 0
+    subnets.each do |subnet|
+
+      describe subnet(subnet['name']) do
+        it { should_not exist }
+      end
+
+    end
+  else
+    fail "A subnet must be defined in the config"
+  end
+end
+
